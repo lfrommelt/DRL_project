1a046462b9d43fbb718c9a149a6499f597025063
Pwachter <38214386+Pwachter@users.noreply.github.com>
2022-09-14 19:06:27 +0200

    cleaned plot function
    the plot function was still able to deal with mnist data and single symbol messages, I deleted this parts. I renamed "plot" to "get output", because it gets the output from the game. "plot2" actually did the plotting, so I renamed it "plot"

--- rlg/game.py
+++ rlg/game.py
@@ -8,6 +8,8 @@ from rlg.trainer_fix import FixedTrainer
 from torch.nn import Module
 from torch.optim import Adam
 from torch import save
+import torch
+import matplotlib.pyplot as plt
 
 #import argparse
 
@@ -34,7 +36,7 @@ class LanguageGame(core.SenderReceiverRnnReinforce):
                 self,
                 sender: Module,
                 receiver: Module,
-                sender_entropy_coeff: float = 0.0,
+                sender_entropy_coeff: float = 0.005,
                 receiver_entropy_coeff: float = 0.0,
                 length_cost: float = 0.0,
                 ):
@@ -77,79 +79,28 @@ class LanguageGame(core.SenderReceiverRnnReinforce):
         save(self.state_dict(), 'models/reinf_Own_Game.pth')
 
     # Easy plotting of the images
-    def plot(game, test_dataset, is_gs, variable_length, is_mnist):
+    def get_output(self, _, test_dataset):
         interaction = \
-            core.dump_interactions(game, test_dataset, is_gs, variable_length)
+            core.dump_interactions(self, test_dataset, gs=False, variable_length=True)
 
         print(interaction.message)
         plots = []
         titles = []
-        for z in range(10):
-            if not is_mnist and not is_gs:
-               # print(np.array(interaction.sender_input[z]).shape)
-               # print(np.array(interaction.receiver_output[z]).shape)
-                src = interaction.sender_input[z].permute(1, 2, 0)
-                dst = interaction.receiver_output[z].view(3, 100, 100).permute(1, 2, 0)
-
-            else:
-                src = interaction.sender_input[z].permute(1, 2, 0)
-                dst = interaction.receiver_output[z].view(-1, 3, 100, 100)
-                dst = dst[-1]
-                dst2 = dst[:-1]
-                dst = dst.permute(1, 2, 0)
-            if is_gs:
-                interaction_message = interaction.message[z]
-            elif not is_mnist and not is_gs:
-                interaction_message = interaction.message[z]
-            else:
-                interaction_message = (
-                    f"Input: digit {z}, channel message tensor({torch.argmax(interaction.message[z], dim=1)})")
+        for z in range(8):
+            src = interaction.sender_input[z].permute(1, 2, 0)
+            dst = interaction.receiver_output[z].view(3, 100, 100).permute(1, 2, 0)
+            interaction_message = interaction.message[z]
 
             image = torch.cat([src, dst], dim=1).cpu().numpy()
             title = (f"Input: digit {z}, channel message {interaction_message}")
             plt.title = title
             plots.append(image)
             titles.append(title)
+
         return plots, titles
 
-    def plot(self, name = "reinf_Own_Game"):
-        plots_game_lstm_Cifar10_GS, titles_game_lstm_Cifar10_GS= _plot(game_lstm_Cifar10_GS, train_data_loader, is_gs=False,
-                                                                       variable_length=True, is_mnist=False)
-        """
-        for i in dst2:
-            img = i.permute(1, 2, 0)
-            img = img*255
-            plt.imshow(img)
-            plt.savefig('reinf_Own_Game_train_other_outputs_' + str(i) + '.png')
-        """
-        plots = []
-        titles = []
-        for i in range(10):
-            plots.append(plots_game_lstm_Cifar10_GS[i])
-            titles.append(titles_game_lstm_Cifar10_GS[i])
-
-        fig = plt.figure(figsize=(100, 100))
-        fig.tight_layout()
-        columns = 1
-        rows = 10
-        for i in range(1, columns * rows + 1):
-            img = plots[i - 1]
-            img = img #* 255
-            title = titles[i - 1]
-            fig.add_subplot(rows, columns, i)
-            fig.tight_layout()
-            plt.gca().set_title(title)
-            plt.imshow(img)
-
-        for i in fig.axes:
-            i.set_xticks([])
-            i.set_yticks([])
-
-        plt.savefig(name+'_train.png')
-
-        # create plot of input and output images from test set
-        plots_game_lstm_Cifar10_GS, titles_game_lstm_Cifar10_GS = plot(game_lstm_Cifar10_GS, test_data_loader, is_gs=False,
-                                                                       variable_length=True, is_mnist=False)
+    def plot(self, test_data, name="reinf_Own_Game"):
+        plots_game_lstm_Cifar10_GS, titles_game_lstm_Cifar10_GS = self.get_output(self, test_data)
 
         plots = []
         titles = []
@@ -163,7 +114,7 @@ class LanguageGame(core.SenderReceiverRnnReinforce):
         rows = 10
         for i in range(1, columns * rows + 1):
             img = plots[i - 1]
-            img = img #* 255
+            img = img
             title = titles[i - 1]
             fig.add_subplot(rows, columns, i)
             fig.tight_layout()
@@ -173,5 +124,5 @@ class LanguageGame(core.SenderReceiverRnnReinforce):
         for i in fig.axes:
             i.set_xticks([])
             i.set_yticks([])
-        #torch.save(game_lstm_Cifar10_GS.state_dict(), '/home/ui/Downloads/reinf_Own_Game.pth')
-        plt.savefig(name+'_test.png')
+
+        plt.savefig(name + '_train.png')
