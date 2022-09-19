e175f23f03d315dbd52ae9d925a4ace0b14ab237
lfrommelt <lfrommelt@uos.de>
2022-09-19 19:12:45 +0200

    beautify

--- main.py
+++ main.py
@@ -2,31 +2,48 @@ import sys
 import rlg
 from egg import core
 import torch
+
 def main(args):
 
-    core.util.init() # most important line of code
+    # initialize egg
+    core.util.init()
 
-    vision = rlg.Vision()
-    class_prediction = rlg.PretrainVision(vision)
-    class_prediction.load_state_dict(torch.load('./models/class_prediction.pth', map_location=torch.device('cpu')))
-    # Agent's and game's setup
-    sender = rlg.SenderCifar10(class_prediction.vision_module)
-    receiver= rlg.ReceiverCifar10()
+    device = core.get_opts().device
 
-    game = rlg.LanguageGame(sender, receiver)#, sender_entropy_coeff=0.005, receiver_entropy_coeff=0.000)
-  #  game.load_state_dict(torch.load('./models/reinf_Own_Game_230_epochs_256_128.pth'))
-    train_data_loader1,train_data_loader2,train_data_loader3, test_data_loader = rlg.load_dataset()
+    # initialize vision module
+    class_prediction = rlg.PretrainVision(rlg.Vision())
+    optimizer = core.build_optimizer(class_prediction.parameters())
+    class_prediction = class_prediction.to(device)
 
-    # train for 3*20 epochs and automatically save resulting model in default way
-    game.train2(20, train_data_loader1, test_data_loader)
-    del(train_data_loader1)
-    game.train2(20, train_data_loader2, test_data_loader)
-    del (train_data_loader2)
-    game.train2(20, train_data_loader3, test_data_loader)
-    EntropyLogger.entropy_log
+    train_data_loader, test_data_loader = rlg.load_dataset((5000,100))
 
-   # game.plot(test_data_loader)
+    for epoch in range(10):
+        mean_loss, n_batches = 0, 0
+        for batch_idx, (data, target) in enumerate(train_data_loader):
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            output = class_prediction(data)
+            loss = torch.nn.functional.l1_loss(output, target)
+            loss.backward()
+            optimizer.step()
+            mean_loss += loss.mean().item()
+            n_batches += 1
 
-print('hello')
+        print(f'Train Epoch: {epoch}, mean loss: {mean_loss / n_batches}')
+
+    class_prediction.save("newly_trained")
+    
+    # initialize game
+    
+    # pass only the vision module, the rest was for pretraining
+    sender = rlg.Sender(class_prediction.vision_module)
+    receiver = rlg.Receiver()
+    game = rlg.LanguageGame(sender, receiver)
+    game.train2(10, train_data_loader, test_data_loader)
+    
+    print("Look, how the entropy changed during training! Low entropy means, that the sender is pretty confident in its messages\n", rlg.EntropyLogger.entropy_log)
+    
+    # todo: plot one example image
+
 if __name__ == "__main__":
     main(sys.argv)
